91955cc7477d3972f18ce55a257cd6f8c1f0b210
Mrinal devnath <codemaxter473@gmail.com>
2025-06-17 15:51:28 +0000

    var

---
+++ javaS/variable.js
@@ -0,0 +1,65 @@
+// variables are named containers used to store data values. They are fundamental building blocks for managing and manipulating information within a program. 
+
+x = 5; 
+
+y = 20;
+
+// console.log(y + x); 
+
+// html >>> 
+
+// document.getElementById("head").style.color = "white";
+
+// document.getElementById("head").style.background = "purple";
+
+// document.getElementById("head").style.padding = "23px"
+
+// let heading1 = document.getElementById("head")
+
+// heading1.addEventListener('mouseover', function purple(){
+//     heading1.style.background = "green";
+// })
+
+// heading1.addEventListener('mouseleave', function purple(){
+//     heading1.style.background = "purple";
+// })
+
+// heading1.addEventListener('click', function purple(){
+//     heading1.style.background = "red";
+// })
+
+
+// heading1.style.transition = "all ease-in-out 0.3s"; 
+
+// heading1.style.cursor = "pointer"
+
+// identifier  = >
+// JavaScript Identifiers
+
+// All JavaScript variables must be identified with unique names.
+
+// These unique names are called identifiers.
+
+// Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
+
+// The general rules for constructing names for variables (unique identifiers) are:
+
+// Names can contain letters, digits, underscores, and dollar signs.
+// Names must begin with a letter.
+// Names can also begin with $ and _ (but we will not use it in this tutorial).
+// Names are case sensitive (y and Y are different variables).
+// Reserved words (like JavaScript keywords) cannot be used as names.
+
+// C.R.U.D => Create Read Update Delete
+
+const firstName = "Mrinal "; // Create
+
+let lastName = "Devanth";
+
+console.log("My name is " , firstName + lastName) // output 1 
+
+firstName = "Shaurya ";  // Update
+
+console.log("My name is " , firstName + lastName) //output 2 
+
+// let, const, var =>
