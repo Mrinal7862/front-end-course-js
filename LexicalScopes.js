564b1ec3e9de3417fc610fae346ed43fcc1bef00
Mrinal devnath <codemaxter473@gmail.com>
2025-06-28 15:26:15 +0000

    thisNewCommit

---
+++ LexicalScopes.js
@@ -0,0 +1,62 @@
+// shamshad - Head boy of the school 
+// 1 2 3 4 5 6 teachers section 
+
+// shamshad principle ya director
+
+// principle can go to directors office but after informing...
+// director can go any where  
+
+// Global scope => iss scope ko program ke outer ya parent portion pe run karenge 
+// let myName = "Mrinal "; 
+
+// for(let i = 0; i<=10; i++){
+//     myName += i
+//     for(let j=0; j<=10; j++){
+//         myName += j;
+//     }
+// }
+
+// if(myName){
+//     console.log(myName)
+// }
+
+// console.log(myName)
+
+// local scope
+
+// if(true){
+//     let num = 1; 
+//     for(let i=num; i<10; i++){
+//         num *= i
+//     }
+//     console.log(num)
+// }
+
+
+// Nested scope
+// if(true){
+//     let num = 1; 
+//     for(let i=num; i<10; i++){
+//         num *= i
+//         console.log(num)
+//     }
+// }
+
+// for(let i=0; i<=10; i++){
+//     for(let j=0; j<=i; j++){
+//         console.log("*")
+//     }
+//     // console.log("\n")
+// }
+
+// Block scope
+// let num = 2
+
+// if(true){
+//     var num2 = 5;
+//     num2 = 7
+//     // console.log(num2)
+// }
+// // console.log(num2)
+// console.log(num2)
+// // console.log(num2)
