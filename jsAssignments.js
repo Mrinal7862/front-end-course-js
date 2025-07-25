3c37e9ad4b303ad18bea57e3b1d78e2e727c37db
Mrinal devnath <codemaxter473@gmail.com>
2025-07-25 14:47:08 +0000

    Assignments

---
+++ jsAssignments.js
@@ -0,0 +1,14 @@
+// 1. Create variables using let, const, and var. Assign values of all data types (string, number,
+// boolean, undefined, null, object, array).
+
+let Name = "Mrinal"
+let num = 3
+// .....
+let obj = {
+    naam : "Mrinal",
+    age : 20,
+    course: "Btech",
+    branch: "AI/ML"
+}
+
+console.log(object.course)
