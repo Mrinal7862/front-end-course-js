367442fbeab2b90650ef087e6cbe3d49b8fc6208
Mrinal devnath <codemaxter473@gmail.com>
2025-07-29 15:46:25 +0000

    DocumentObjModel

---
+++ DOM/script.js
@@ -0,0 +1,25 @@
+// The Document Object Model (DOM) in JavaScript is a programming interface for HTML and XML documents. It represents the structure of a document as a logical tree, where each part of the document (like elements, attributes, and text) is represented as an object, referred to as a node.
+
+
+
+const headingStyle = document.getElementById("heading").style;
+
+
+headingStyle.backgroundColor = "red"
+
+const heading = document.getElementById("heading")
+
+
+// text = ""
+
+// for(let i = 1; i<=5; i++){
+//      text += i + "<br>";
+// }
+// headingStyle.color = 'white'
+// heading.innerHTML = text
+
+heading.innerHTML = "<h2>I'm very good person</h2>"
+
+const foot  = document.getElementById("footer")
+
+foot.innerHTML = "<p>I'm very good person</p>"
